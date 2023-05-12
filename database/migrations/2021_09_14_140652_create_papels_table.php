94fb6cf20ba5bf99307173a3813abdb85ec8e508
Caio Vinicius Rodrigues <dti-244@pmscs.local>
2023-05-12 16:28:05 -0300

    alteracaobanco4

--- database/migrations/2021_09_14_140652_create_papels_table.php
+++ database/migrations/2021_09_14_140652_create_papels_table.php
@@ -14,7 +14,7 @@ class CreatePapelsTable extends Migration
     public function up()
     {
         Schema::create('papeis', function (Blueprint $table) {
-            $table->increments('id');
+            $table->increments('id')->unsigned();
             $table->string("nome");
             $table->string("descricao")->nullable();
             $table->timestamps();
